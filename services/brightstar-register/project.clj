cdaf6638f5bec5e228108cb00b3eb1f16f9bca98
Jonathan Pelletier <maximusmagnusjopela@gmail.com>
2015-09-18 12:34:08 -0400

    Adding some command line argument parsing (help + port)

--- services/brightstar-register/project.clj
+++ services/brightstar-register/project.clj
@@ -3,6 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.cli "0.3.3"]
                  [http-kit "2.1.18"]
                  [compojure "1.4.0"]
                  [hiccup "1.0.5"]]
--- services/brightstar-register/src/brightstar_register/core.clj
+++ services/brightstar-register/src/brightstar_register/core.clj
@@ -1,9 +1,27 @@
 (ns brightstar-register.core
   (:require [org.httpkit.server :refer [run-server]]
-            [brightstar-register.routes :refer [brightstar-routes]])
+            [brightstar-register.routes :refer [brightstar-routes]]
+            [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
+
+(def cli-opts
+  [["-h" "--help" "prints this usage summary and quit"]
+   ["-p" "--port PORT" "listening port of the"
+    :default 8080
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 65536) "port must be an integer greater then 0 and less then 65536"]]])
+
 
 (defn -main
   "brightstar registration service"
   [& args]
-  (run-server brightstar-routes {:port 8080}))
+  (let [{:keys [arguments options errors summary]} (parse-opts args cli-opts)]
+    (when (options :help)
+      (println summary)
+      (System/exit 0))
+    (when errors
+      (doseq [e errors]
+        (println e)
+        (System/exit -1)))
+    (let [{:keys [port]} options]
+      (run-server brightstar-routes {:port port}))))
